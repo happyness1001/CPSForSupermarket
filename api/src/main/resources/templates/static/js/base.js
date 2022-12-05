d5f55a392699af0d28ad56d7daf35910c9a40dcd
zhaishangzhao <61646076+zhaishangzhao@users.noreply.github.com>
2022-12-05 09:56:16 +0800

    update

--- api/src/main/resources/templates/static/js/base.js
+++ api/src/main/resources/templates/static/js/base.js
@@ -1,6 +1,8 @@
 
 // var baseUrl = "http://123.249.18.188:8080/";
 // var webSocketBaseUrl = "ws://123.249.18.188:8080/"
-
+// var cpsUrl = "http://123.249.18.188:80/";
 var baseUrl = "http://localhost:8080/";
-var webSocketBaseUrl = "ws://localhost:8080/"
+var webSocketBaseUrl = "ws://localhost:8080/";
+var cpsUrl = "http://localhost:80/";
+
